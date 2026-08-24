78ae9e145420ff8d96d7c5c1ec599ea8f48ddbd4
Gholamreza.Rezaei <ghrezaei1399@gmail.com>
2026-08-24 18:15:07 +0330

    Update register.js

--- api/register.js
+++ api/register.js
@@ -2,6 +2,7 @@ export default async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
+
     try {
         const userData = req.body;
 
