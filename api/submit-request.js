3358c976d2afd35f90334cae1ebc8b7247cba4a2
Gholamreza.Rezaei <ghrezaei1399@gmail.com>
2026-08-31 21:15:32 +0330

    Update submit-request.js

--- api/submit-request.js
+++ api/submit-request.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
@@ -183,4 +183,4 @@ export default async function handler(req, res) {
     console.error('Submit Request Error:', error);
     return res.status(500).json({ error: error.message });
   }
-}
+};
