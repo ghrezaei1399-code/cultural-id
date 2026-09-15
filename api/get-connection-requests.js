645756741e6cbe239160af73d705e08bbbd04aa3
Gholamreza.Rezaei <ghrezaei1399@gmail.com>
2026-09-15 21:34:38 +0330

    Update get-connection-requests.js

--- api/get-connection-requests.js
+++ api/get-connection-requests.js
@@ -91,7 +91,7 @@ module.exports = async function handler(req, res) {
 
     // ===== مشاهدات =====
     if (type === 'observations') {
-      const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/issues?labels=observation&state=open&per_page=100`, {
+      const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/issues?labels=observation&state=all&per_page=100`, {
         headers: {
           'Authorization': `Bearer ${token}`,
           'Accept': 'application/vnd.github.v3+json'
